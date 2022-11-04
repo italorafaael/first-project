f9c102abd2bff3fc6dee61a7a54d96d81ace4c9c
Italo Rafael <italorafael.dev@gmail.com>
2022-11-04 01:37:14 -0300

    fourth steps in java with spring boot

--- JavaWithSpringBoot/first-project/src/main/java/br/com/first/controllers/PersonController.java
+++ JavaWithSpringBoot/first-project/src/main/java/br/com/first/controllers/PersonController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.first.model.Person;
+import br.com.first.data.vo.v1.PersonVO;
 import br.com.first.services.PersonService;
 
 @RestController
@@ -25,22 +25,22 @@ public class PersonController {
 	private PersonService service;
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
+	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
 
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") Long id) {
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
 		return service.findById(id);
 	}
 
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create(@RequestBody Person person) {
+	public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
 	}
 
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody Person person) {
+	public PersonVO update(@RequestBody PersonVO person) {
 		return service.update(person);
 	}
 
